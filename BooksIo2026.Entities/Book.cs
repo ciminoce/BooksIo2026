f7335e236b9d9227c45f0b95220ad56a1a15dfb5
ciminoce <ciminoce@yahoo.com.ar>
2026-04-24 17:53:54 -0300

    ultima version

--- BooksIo2026.Entities/Book.cs
+++ BooksIo2026.Entities/Book.cs
@@ -9,15 +9,11 @@ namespace BooksIo2026.Entities
         public int BookId { get; set; }
 
         public string Title { get; set; } = null!;
-
         public int AuthorId { get; set; }
         public Author Author { get; set; } = null!;
-
         public int PublisherId { get; set; }
         public Publisher Publisher { get; set; } = null!;
-
         public DateTime PublishedDate { get; set; }
-
         public decimal Price { get; set; }
         public int Stock { get; set; }
 
